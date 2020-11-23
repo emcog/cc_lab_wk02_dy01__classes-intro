158ac9f4eefb083469faea89036cdd07229771ad
emCog <robin.leonard@gmail.com>
2020-11-23 16:03:22 +0000

    passed 08

--- single_class_lab_start_code/team_class/src/team.py
+++ single_class_lab_start_code/team_class/src/team.py
@@ -2,7 +2,8 @@ class Team:
     def __init__(self, name, players, coach):
         self.name = name
         self.players = players
-        self.coach = coach 
+        self.coach = coach
+        self.points = 0
 
     def add_player(self, player):
         self.players.append(player)
